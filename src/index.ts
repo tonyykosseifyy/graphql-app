1d22d22f7774f5a12ebf85778782c40d3c1ac461
Tony Kosseify <tonykosseify123@gmail.com>
2024-04-09 00:16:37 +0300

    create: typeDefs

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,7 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 
-const typeDefs = `#graphql
-  type Book {
-    title: String
-    author: String
-  }
-
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
-  type Query {
-    books: [Book]
-  }
-`;
+import { typeDefs } from './schema';
 
 
 const server = new ApolloServer({
