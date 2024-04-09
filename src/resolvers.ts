6475c776578e88908afb0fb618a8b5f29519981f
Tony Kosseify <tonykosseify123@gmail.com>
2024-04-09 12:23:44 +0300

    add: data relationships

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -9,7 +9,16 @@ const resolvers = {
     game: (_, { id }) => data.games.find((game) => game.id === id),
     author: (_, { id }) => data.authors.find((author) => author.id === id),
   },
+  Game: {
+    reviews: (game) => data.reviews.filter((review) => review.game_id === game.id),
+  },
+  Review: {
+    game: (review) => data.games.find((game) => game.id === review.game_id),
+    author: (review) => data.authors.find((author) => author.id === review.author_id),
+  },
+  Author: {
+    reviews: (author) => data.reviews.filter((review) => review.author_id === author.id),
+  },
 };
-
 
 export default resolvers;
--- src/schema.ts
+++ src/schema.ts
@@ -2,19 +2,23 @@ export const typeDefs = `#graphql
     type Game {
         id: ID!,
         title: String!,
-        platform: [String!]!
+        platform: [String!]!,
+        reviews: [Review!]
     }
     
     type Review {
         id: ID!,
         rating: Int!,
         content: String!
+        game: Game!,
+        author: Author!
     }
 
     type Author {
         id: ID!,
         name: String!,
-        verified: Boolean!
+        verified: Boolean!,
+        reviews: [Review!]
     }
 
     type Query {
